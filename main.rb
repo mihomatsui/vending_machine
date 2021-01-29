38e03403e4b71bb19034430310c1306c25849b27
mihomatsui <mihomatsui.24@gmail.com>
2021-01-29 23:58:29 +0900

    商品データを追記

--- main.rb
+++ main.rb
@@ -1,1 +1,12 @@
 puts "お金を投入してください。"
+puts "お金を投入"
+
+# 商品データ
+products = [
+  {name: "ジュース", price:130}
+  {name: "コーラ", price:150}
+  {name: "お茶", price:110}
+  {name: "コーヒー", price:120}
+  {name: "おしるこ", price:90}
+  {name: "スープ", price:100}
+]
