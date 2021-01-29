1aacb90e2212b2f4410239b1ecfeff927b8c57ce
mihomatsui <mihomatsui.24@gmail.com>
2021-01-29 23:59:54 +0900

    コンマを追記

--- main.rb
+++ main.rb
@@ -3,10 +3,10 @@ puts "お金を投入"
 
 # 商品データ
 products = [
-  {name: "ジュース", price:130}
-  {name: "コーラ", price:150}
-  {name: "お茶", price:110}
-  {name: "コーヒー", price:120}
-  {name: "おしるこ", price:90}
+  {name: "ジュース", price:130},
+  {name: "コーラ", price:150},
+  {name: "お茶", price:110},
+  {name: "コーヒー", price:120},
+  {name: "おしるこ", price:90},
   {name: "スープ", price:100}
 ]
